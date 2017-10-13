064ddfee5ae4aa29093929891b938474c31db26a
Matthew Beebe <matthewbeebe@Matthews-MacBook-Pro.local>
2017-10-13 10:45:52 -0400

    Added some missing connection data

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burger_db"
-});
+
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "burger_db"
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
