2541ab115e610684f2915764587b7df208746126
Matthew Beebe <matthewbeebe@Matthews-MacBook-Pro.local>
2017-10-12 21:50:53 -0400

    Working and Styled

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name, eaten) VALUES ('Pizza Burger', false);
+INSERT INTO burgers (name, eaten) VALUES ('Black Bean Burger', false);
+INSERT INTO burgers (name, eaten) VALUES ('Santa Fe Burger', false);
